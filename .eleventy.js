7199de0e8ed7888c2bb62ee8cb177d8c25ada9e1
George Owen <gbo.owen@gmail.com>
2022-11-16 14:29:26 -0800

    images WIP, blog header WIP

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,33 @@
+// Images
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, className = undefined, widths = [400,800,1200], formats=['webp','jpeg'],sizes='100vw') {
+  if(alt === undefined) {
+    // throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+  }
+
+  let metadata = await Image(src, {
+    widths: [...widths,null],
+    formats: [...formats, null],
+    outputDir: "public/assets/images",
+    urlPath: "/assets/images"
+  });
+
+  let data = metadata.jpeg[metadata.jpeg.length - 1];
+  return `<img src="${data.url}" width="${data.width}" height="${data.height}" alt="${alt}">`;
+}
+
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addWatchTarget("./src/css/");
   eleventyConfig.addPassthroughCopy("./src/images");
   eleventyConfig.addWatchTarget("./src/images/");
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addLiquidShortcode("image", imageShortcode);
+  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
 
   eleventyConfig.addCollection("quotes", collectionAPI => {
     const quoteArr = collectionAPI.getFilteredByGlob("./src/blog/*.md");
@@ -23,10 +47,12 @@ module.exports = function (eleventyConfig) {
     return arr[0];
   });
 
+
   return {
     dir: {
       input: "src",
       output: "public",
+      markdownTemplateEngine: "md"
     },
   };
 };
--- src/about.md
+++ src/about.md
@@ -3,4 +3,10 @@ title: About me
 layout: root-page.njk
 ---
 
-Hi! My name is George Owen. I'm a baller boy :)
+{% image "./src/images/go-graduation-stand.jpg", "A graduation photo" %}
+
+Hi! My name is George Owen. I'm a programmer and writer passionate about the physical world we live in. I value creativity and spontaneity
+
+horizontal images: a logistic map zoomed in
+
+finding stable points in a world of chaos
